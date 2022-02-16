f5781bb682a51630d053a9bb1680f0cf5f81bb05
neyneyalldayday <drfrankenstein123@gmail.com>
2022-02-16 12:02:16 -0600

    stuff

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert('you daggy this is an alert! javascript is doing a thingy!!!')
+
+const playerName = window.prompt("what's your daddy Robots name?");
+// window.alert(playerName);
+console.log(playerName);
+
+console.log("this ligs a string, good for leaving yourself a message");
+console.log( 10 + 10);
+//whats this shit?
+console.log(" our robots name is " + playerName);
+
+
+
+function fight() {
+    window.alert("Try and Choke me Daddy!!");
+}
+// fight();
